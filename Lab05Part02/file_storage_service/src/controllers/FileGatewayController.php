f88653b6ea9c066271fed68e00aa6323038db6ce
Nicholas Solomon <nicholassolomon99@outlook.com>
2024-11-08 13:16:58 -0800

    Lab05 Part02 Starter Kit

---
+++ Lab05Part02/file_storage_service/src/controllers/FileGatewayController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace NewCo\FileGateway\Controllers;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class FileGatewayController
+{
+    /**
+     * Accept an image, and save it to the images/ directory on disk.
+     */
+    public function save(Request $request): Response
+    {
+        // TODO
+    }
+
+}
---
+++ Lab05Part02/user_service/src/controllers/UploadController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace NewCo\UserService\Controllers;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UploadController
+{
+    /**
+     * Accept an image upload, extract data from the upload, and pass the image to
+     * localhost:4000 for storage.
+     */
+    public function upload(Request $request): Response
+    {
+        // TODO
+    }
+
+}
